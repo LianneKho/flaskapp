ce51dfd6f89a2e8f804204a92a8d7eaea83d37ee
Lianne <lianne@MacBookPro.localdomain>
2025-01-16 19:36:33 +0100

    Added Flask to requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Render!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))  # Render zal de juiste poort toewijzen
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5001)
