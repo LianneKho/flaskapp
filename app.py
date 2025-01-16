e02d8420183c666cfabd6bb70401b44b97ad4aec
Lianne <lianne@MacBookPro.localdomain>
2025-01-16 19:21:14 +0100

    Omschrijving van de wijziging

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Hello, Render!"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get("PORT", 5001))  # Render zal de juiste poort toewijzen
+    app.run(debug=True, host='0.0.0.0', port=port)
