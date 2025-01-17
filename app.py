a541f6ae589d5f4e0dc4a365ef3d213ccac9e221
Lianne <lianne@MacBookPro.localdomain>
2025-01-17 16:37:51 +0100

    Fix static file links

--- app.py
+++ app.py
@@ -15,5 +15,7 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5001)
+    
+
     
