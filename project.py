7f5b4e02aa3a71a36da75b9cbb5b916d84fc3bbe
Lianne <lianne@MacBookPro.localdomain>
2025-02-28 18:22:00 +0100

    Updated main function

--- project.py
+++ project.py
@@ -181,5 +181,7 @@ def register():
         return redirect(url_for('voortgang'))
 
     return render_template('register.html')
+def main():
+    app.run(debug=True, host='0.0.0.0', port=5001)
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
